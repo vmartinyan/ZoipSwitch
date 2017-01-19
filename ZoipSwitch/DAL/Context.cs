3d9cf92681f1a20e3caba716b8f6ee12bc0b1f34
Vahram Martinyan <vahram.martinyan@hotmail.com>
2017-01-19 18:43:34 +0400

    19-01-2017

---
+++ ZoipSwitch/DAL/Context.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+using ZoipSwitch.Models;
+
+namespace ZoipSwitch.DAL
+{
+    public class Context : DbContext
+    {
+        public Context() : base("DefaultConnection")
+        {
+        }
+
+        public DbSet<order_status> Order_Statuses { get; set; }
+
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
---
+++ ZoipSwitch/Models/order_status.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace ZoipSwitch.Models
+{
+    [Table("order_status", Schema = "public")]
+    public class order_status
+    {
+        [Key]
+        public int order_status_flags { get; set; }
+
+        public string order_status_text { get; set; }
+    }
+}
