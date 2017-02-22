77cacb4c67145e85dbaa1debc34cc4e82ffc06f4
Vahram Martinyan <vahram.martinyan@hotmail.com>
2017-02-22 11:44:45 +0400

    22-02-2017

---
+++ ZoipSwitch/Models/residents.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace ZoipSwitch.Models
+{
+    [Table("residents", Schema = "public")]
+    public class residents
+    {
+        [Key]
+        public int resident_id { get; set; }
+
+        [Display(Name = "Name")]
+        public string resident_name { get; set; }
+
+        [Display(Name = "Lastname")]
+        public string resident_lastname { get; set; }
+
+        [Display(Name = "Birth Date")]
+        public DataType resident_birthdate { get; set; }
+
+        [Display(Name = "Phone")]
+        public string resident_phone { get; set; }
+
+        [Display(Name = "Email")]
+        public string resident_email { get; set; }
+    }
+}
